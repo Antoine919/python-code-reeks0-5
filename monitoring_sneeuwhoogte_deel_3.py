48c9a49418f5cb583ee7e318b981a10c23a3f51c
Antoine919 <antoineguiot015@gmail.com>
2025-05-20 17:49:58 +0200

    Add files via upload

--- monitoring_sneeuwhoogte_deel_3.py
+++ monitoring_sneeuwhoogte_deel_3.py
@@ -31,7 +31,7 @@ def lijsten_maken (lengtegraden_csv, breedtegraden_csv, bestand = None):
     else:
         return lengte_lijst, breedte_lijst
 
-
+# het is best om 1 functie te maken die coördinaten zoekt en de echte coord ook als argument te geven, als input
 def coord_alpe(lengtegraden_csv, breedtegraden_csv):
     """Bepaalt welke OL en NB (en de bijhorende index) uit de gegeven csv-bestanden er het dichtst bij de coördinaten van het skigebied L’Alpe de Vénosc (44,9467 NB en 6,1883 OL) liggen. """
     lengte_lijst, breedte_lijst = lijsten_maken(lengtegraden_csv, breedtegraden_csv)
@@ -105,10 +105,10 @@ def sneeuwhoogte_sat (sat_geg, lengtegraden_csv, breedtegraden_csv):
 station 7893 voor Courchevel voor de maand januari a.d.h.v. de gegeven csv-bestanden. Gebruikt de dichtstbijzijnde OL en NB."""
     lijst_van_lijsten = lijsten_maken(lengtegraden_csv, breedtegraden_csv, sat_geg)[2]
     ind_NB_C, NB_sat_C, ind_OL_C, OL_sat_C = coord_courchevel(lengtegraden_csv, breedtegraden_csv)
-    sneeuwhoogte_C = lijst_van_lijsten[ind_OL_C][ind_NB_C]
+    sneeuwhoogte_C = lijst_van_lijsten[ind_NB_C][ind_OL_C]
     ind_NB_A, NB_sat_A, ind_OL_A, OL_sat_A = coord_alpe(lengtegraden_csv, breedtegraden_csv)
-    sneeuwhoogte_A = lijst_van_lijsten[ind_OL_A][ind_NB_A]
-    return sneeuwhoogte_C, sneeuwhoogte_A
+    sneeuwhoogte_A = lijst_van_lijsten[ind_NB_A][ind_OL_A]
+    return float(sneeuwhoogte_C), float(sneeuwhoogte_A)
 
 
 def gem_sneeuw_januari_weerstat (stat_bestand_1, stat_bestand_2):
@@ -133,6 +133,7 @@ def heatmap (sat_geg, lengtegraden_csv, breedtegraden_csv):
 
 
 # tests
+"""
 print(coord_alpe("lon.csv", "lat.csv"))
 print()
 print(coord_courchevel("lon.csv", "lat.csv"))
@@ -141,3 +142,4 @@ print(sneeuwhoogte_sat("jan_Sentinel.csv", "lon.csv", "lat.csv",))
 print()
 print(gem_sneeuw_januari_weerstat("7893_datums_Courchevel.csv","7589_datums_L'Alpe_de_Vénosc.csv"))
 heatmap("jan_Sentinel.csv", "lon.csv", "lat.csv")
+"""
--- monitoring_sneeuwhoogte_deel_4.py
+++ monitoring_sneeuwhoogte_deel_4.py
@@ -3,6 +3,95 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import csv
+import numpy as np
+from monitoring_sneeuwhoogte_deel_1 import aangepaste_gegevens
+from monitoring_sneeuwhoogte_deel_3 import sneeuwhoogte_sat
+
+
+def verschil_staafdiagram_slecht (station_C, station_A, lat, lon, *maanden):
+    """Deze functie geeft een staafdiagram met de verschillen tussen de waarden van de satelliet en de lokale
+weerstations, maar deze werkt niet."""
+    station_C_df = aangepaste_gegevens(station_C)
+    station_A_df = aangepaste_gegevens(station_A)
+    verschillen_dict_C = dict()
+    verschillen_dict_A = dict()
+    count = 0
+    d = 31
+    m = 10
+    j = 2019
+    for bestand in maanden:
+        maand = bestand[0:3]
+        sneeuwhoogte_C_sat, sneeuwhoogte_A_sat = sneeuwhoogte_sat(bestand, lon, lat)
+        sneeuwhoogte_C_stat = station_C_df.Sneeuwhoogte[f"1/{m}/{j}":f"{d}/{m}/{j}"].mean()
+        sneeuwhoogte_A_stat = station_A_df.Sneeuwhoogte[f"1/{m}/{j}":f"{d}/{m}/{j}"].mean()
+        verschillen_dict_C[maand] = sneeuwhoogte_C_stat - float(sneeuwhoogte_C_sat)
+        verschillen_dict_A[maand] = sneeuwhoogte_A_stat - float(sneeuwhoogte_A_sat)
+        count +=1
+        if count == 1:
+            d = 30
+            m += 1
+        elif count == 2:
+            d = 31
+            m += 1
+        elif count == 3:
+            m = "01"
+            j = 2020
+        elif count == 4:
+            d = 29
+            m = "02"
+
+    X_as = np.array(5)
+    plt.bar(X_as, verschillen_dict_C.values, label = "Courchevel")
+    plt.bar(X_as, verschillen_dict_A.values, label ="Alpe de Vénosc")
+    plt.legend()
+    plt.title("Verschil in sneeuwdiepte tussen het weerstation en de satelliet ")
+    plt.show()
+
+def gem_sneeuw_maand (weerstation, maand):
+    """Deze functie zoekt de gemiddelde temperatuur van de gegeven maand."""
+    maanden_dict = {"jan":1, "feb":2, "maa":3, "apr":4, "mei":5, "jun":6, "jul":7, "aug":8, "sep":9, "okt":10, "nov":11, "dec":12}
+    weergegevens_df = aangepaste_gegevens(weerstation)
+    maand_getal = maanden_dict[maand[0:3].lower().strip()]
+    maand_sneeuw = []
+    for index in weergegevens_df.index:
+        d, m, j = index.split("/")
+        m = int(m)
+        if m == maand_getal:
+            maand_sneeuw.append(weergegevens_df.loc[index, "Sneeuwhoogte"])   # !!!
+    gem_sneeuw = np.nansum(maand_sneeuw)/ len(maand_sneeuw)
+    return gem_sneeuw
+
+
+def verschillen_lijsten (weerstation_1, weerstation_2, lon, lat, sat_geg):
+    maanden_lijst = []
+    verschillen_lijst_1 = []
+    verschillen_lijst_2 = []
+    for bestand in sat_geg:
+        maanden_lijst.append(bestand[0:3])
+        gem_sneeuw_1 = gem_sneeuw_maand(weerstation_1, bestand[0:3])
+        gem_sneeuw_2 = gem_sneeuw_maand(weerstation_2, bestand[0:3])
+        sneeuw_1, sneeuw_2 = sneeuwhoogte_sat(bestand,lon, lat)
+        verschillen_lijst_1.append(gem_sneeuw_1 - sneeuw_1)
+        print(f"verschil C: {gem_sneeuw_1 - sneeuw_1}")
+        verschillen_lijst_2.append(gem_sneeuw_2 - sneeuw_2)
+        print(f"verschil A: {gem_sneeuw_2 - sneeuw_2}")
+        print()
+    return maanden_lijst, verschillen_lijst_1, verschillen_lijst_2
+
+
+def verschillen_staafdiagram (weerstation_1, weerstation_2, lon, lat, *sat_geg):
+    maanden, lijst_1, lijst_2 = verschillen_lijsten(weerstation_1, weerstation_2, lon, lat, sat_geg)
+    x_as = np.arange(len(maanden))
+    width = 0.3
+    plt.figure(1)
+    plt.bar(x_as, lijst_1, width = width, label = "Courchevel")
+    plt.bar(x_as + width, lijst_2, width = width, label = "Alpe de Vénosc")
+    plt.legend()
+    plt.xticks(x_as, maanden)
+    plt.xlabel("Maand")
+    plt.ylabel("verschil in sneeuwhoogte (m)")
+    plt.title("Verschil in sneeuwhoogtes")
+    plt.show(block = False)
 
 
 def heatmap_meerdere_maanden ():
@@ -26,6 +115,7 @@ def heatmap_meerdere_maanden ():
     nummer = 0
     while 0 < 1 : # oneindige loop maken
         for maand in maanden_files:
+            plt.figure(2)
             #plt.figure(maand) # zodat elke grafiek apart wordt afgebeeld
             df_maand = pd.read_csv(maand, names = lengte_graden_lijst)
             df_maand.index = breedtegraden_lijst
@@ -39,9 +129,14 @@ def heatmap_meerdere_maanden ():
             nummer += 1
     plt.show()  #zorgt ervoor dat de figuren blijven openstaan
 
-print(heatmap_meerdere_maanden())
+#verschil_staafdiagram_slecht("7893_datums_Courchevel.csv", "7589_datums_L'Alpe_de_Vénosc.csv", "lat.csv", "lon.csv", "okt_Sentinel.csv", "nov_Sentinel.csv", "dec_Sentinel.csv", "jan_Sentinel.csv", "feb_Sentinel.csv")
+verschillen_staafdiagram("7893_datums_Courchevel.csv", "7589_datums_L'Alpe_de_Vénosc.csv",  "lon.csv", "lat.csv","okt_Sentinel.csv", "nov_Sentinel.csv", "dec_Sentinel.csv", "jan_Sentinel.csv", "feb_Sentinel.csv")
+heatmap_meerdere_maanden()
+
 #inputs:
 """
+7893_datums_Courchevel.csv
+7589_datums_L'Alpe_de_Vénosc.csv
 okt_Sentinel.csv, nov_Sentinel.csv, dec_Sentinel.csv, jan_Sentinel.csv, feb_Sentinel.csv
 lat.csv
 lon.csv
